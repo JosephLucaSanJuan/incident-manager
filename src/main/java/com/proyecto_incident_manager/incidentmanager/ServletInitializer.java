e09ac1b5520ef1367f38fd590d43cf0bbae55bfd
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-01-15 10:19:24 +0100

    Proyecto_Incident_Manage

---
+++ src/main/java/com/proyecto_incident_manager/incidentmanager/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.proyecto_incident_manager.incidentmanager;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IncidentManagerApplication.class);
+	}
+
+}
---
+++ src/test/java/com/proyecto_incident_manager/incidentmanager/IncidentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_incident_manager.incidentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
